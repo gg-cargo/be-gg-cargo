8b9639949e9f75965df9f76e73d06e5f03dfc296
Haikal-cyber <fikri.haikal234@gmail.com>
2025-11-14 21:18:51 +0700

    refactor: update ops/order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5241,7 +5241,7 @@ export class OrdersService {
                             [Op.and]: [
                                 { reweight_status: 0 },
                                 { issetManifest_inbound: 1 },
-                                { [Op.or]: [{ status_pickup: 'Picked Up' }, { status_pickup: null }] }
+                                { [Op.or]: [{ status_pickup: 'Picked Up' }, { status_pickup: null }, { status_pickup: 'Completed' }] }
                             ]
                         };
                         break;
