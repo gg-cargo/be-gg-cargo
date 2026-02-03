f3dfecc6d41c839b744dc88a433c56763de2617b
Haikal-cyber <fikri.haikal234@gmail.com>
2026-02-03 10:00:22 +0700

    fix: fix error cors

--- src/master-routes/master-routes.module.ts
+++ src/master-routes/master-routes.module.ts
@@ -5,10 +5,11 @@ import { MasterRoutesService } from './master-routes.service';
 import { MasterRoute } from '../models/master-route.model';
 import { RouteGate } from '../models/route-gate.model';
 import { RoutePolyline } from '../models/route-polyline.model';
+import { MasterRouteGate } from '../models/master-route-gate.model';
 import { RouteGatesModule } from '../route-gates/route-gates.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([MasterRoute, RouteGate, RoutePolyline]), RouteGatesModule],
+  imports: [SequelizeModule.forFeature([MasterRoute, RouteGate, RoutePolyline, MasterRouteGate]), RouteGatesModule],
   controllers: [MasterRoutesController],
   providers: [MasterRoutesService],
   exports: [MasterRoutesService],
--- src/route-gates/route-gates.module.ts
+++ src/route-gates/route-gates.module.ts
@@ -4,9 +4,10 @@ import { RouteGatesController } from './route-gates.controller';
 import { RouteGatesService } from './route-gates.service';
 import { RouteGate } from '../models/route-gate.model';
 import { MasterRoute } from '../models/master-route.model';
+import { MasterRouteGate } from '../models/master-route-gate.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([RouteGate, MasterRoute])],
+  imports: [SequelizeModule.forFeature([RouteGate, MasterRoute, MasterRouteGate])],
   controllers: [RouteGatesController],
   providers: [RouteGatesService],
   exports: [RouteGatesService],
